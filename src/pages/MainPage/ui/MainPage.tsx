93ae486186641831552982e1d653df8955759a29
Tatiana Wansiedler <tatjana@wansiedler.com>
2024-04-28 12:42:46 +0200

    add ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-    const { t, i18n } = useTranslation('main');
-    return <div>{t('Main Page')}</div>;
+    const { t } = useTranslation('main');
+    return (
+        <div>
+            {t('Main Page')}
+        </div>
+    );
 };
 
 export default MainPage;
