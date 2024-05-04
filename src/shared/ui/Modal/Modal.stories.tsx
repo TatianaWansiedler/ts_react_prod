34845652ecbf6d864db2768ae53e04011dc4325a
Tatiana Wansiedler <tatjana@wansiedler.com>
2024-05-04 12:29:00 +0200

    add husky

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -20,9 +20,9 @@ Primary.args = {
     children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 
-export const Dark = Template.bind({});
-Dark.args = {
-    isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
-};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+// export const Dark = Template.bind({});
+// Dark.args = {
+//     isOpen: true,
+//     children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
+// };
+// Dark.decorators = [ThemeDecorator(Theme.DARK)];
