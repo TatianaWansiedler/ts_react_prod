511d1076e4e131ec3b35f5902a68f870bab1d245
Tatiana Wansiedler <tatjana@wansiedler.com>
2024-05-19 11:45:34 +0200

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,44 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getUserAuthData } from 'entities/User';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/main.svg';
+import ProfileIcon from 'shared/assets/icons/profile.svg';
+import ArticleIcon from 'shared/assets/icons/article.svg';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import type { SidebarItemType } from '../types/sidebar';
+
+export const getSidebarItems = createSelector(
+    getUserAuthData,
+    (userData) => {
+        const sidebarItemsList: SidebarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Home',
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'About us',
+            },
+
+        ];
+        if (userData) {
+            sidebarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Profile',
+                    authOnly: true,
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: ArticleIcon,
+                    text: 'Articles',
+                    authOnly: true,
+                },
+            );
+        }
+        return sidebarItemsList;
+    },
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,9 +3,10 @@ import { memo, useMemo, useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
-import { SidebarItemsList } from '../../model/items';
+import { useSelector } from 'react-redux';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
 import cls from './Sidebar.module.scss';
+import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 
 interface SidebarProps {
   className?: string;
@@ -13,20 +14,20 @@ interface SidebarProps {
 
 export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const sidebarItemsList = useSelector(getSidebarItems);
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
     const itemsList = useMemo(
-        () => SidebarItemsList.map((item) => (
+        () => sidebarItemsList.map((item) => (
             <SidebarItem
                 item={item}
                 collapsed={collapsed}
                 key={item.path}
             />
         )),
-        [collapsed],
+        [collapsed, sidebarItemsList],
     );
 
     return (
